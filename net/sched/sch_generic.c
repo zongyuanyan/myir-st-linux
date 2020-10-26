7a6cb70bf68dfe191b3a7cf25e3f6c77d79e0993
lichang70 <343952147@qq.com>
2020-10-26 09:03:40 +0800

    Just for warnings

--- net/sched/sch_generic.c
+++ net/sched/sch_generic.c
@@ -444,8 +444,10 @@ static void dev_watchdog(struct timer_list *t)
 
 			if (some_queue_timedout) {
 				trace_net_dev_xmit_timeout(dev, i);
+				/* Just for kernel warnings
 				WARN_ONCE(1, KERN_INFO "NETDEV WATCHDOG: %s (%s): transmit queue %u timed out\n",
 				       dev->name, netdev_drivername(dev), i);
+				*/
 				dev->netdev_ops->ndo_tx_timeout(dev);
 			}
 			if (!mod_timer(&dev->watchdog_timer,
